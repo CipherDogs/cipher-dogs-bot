591d9a35402e6e874e1d37beb824281ff1c247da
D34D_FR13ND5 <47918260+ptfn@users.noreply.github.com>
2020-10-30 19:44:52 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,12 +3,12 @@ import telebot
 
 bot = telebot.TeleBot(token = os.getenv('TOKEN'))
 
-@bot.message_handler(content_types=['new_chat_members'])
-def send_sticker(message):
-	username = message.chat.username
-	if username == 'cyber_russian_community':
-		bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAJAXV-ZAjfq6sotbN3e5_Nc-NMc3RWlAAJWAQACK9RLC9RAtYotQ8NPGwQ')
-	elif username == 'fuckgoogle':
-		bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAJAhl-ZZlpBtcyICOlr_VyWthXoch_7AAIYAQACK9RLC7eumetzzfY-GwQ')
+@bot.message_handler(func=lambda message: message.chat.username == 'cyber_russian_community', content_types=['new_chat_members'])
+def send_sticker_ru(message):
+	bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAJAXV-ZAjfq6sotbN3e5_Nc-NMc3RWlAAJWAQACK9RLC9RAtYotQ8NPGwQ')
+
+@bot.message_handler(func=lambda message: message.chat.username == 'fuckgoogle', content_types=['new_chat_members'])
+def send_sticker_en(message):
+	bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAJAhl-ZZlpBtcyICOlr_VyWthXoch_7AAIYAQACK9RLC7eumetzzfY-GwQ')
 
 bot.polling()
