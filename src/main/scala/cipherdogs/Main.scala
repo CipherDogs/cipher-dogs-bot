c93254ac01909c062643fa07746ef74f70893ce3
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-11-26 21:13:49 +0700

    Added greeting

--- src/main/scala/cipherdogs/Main.scala
+++ src/main/scala/cipherdogs/Main.scala
@@ -1,12 +1,42 @@
 package cipherdogs
 
+import canoe.api._
+import canoe.models.messages.{ChatMemberAdded, TelegramMessage}
+import canoe.syntax._
+import cats.effect.concurrent.Semaphore
 import cats.effect.{ExitCode, IO, IOApp}
+import cats.syntax.all._
+import fs2.Stream
 
 object Main extends IOApp {
-  val token: String = "<your telegram token>"
+  val token: String = scala.util.Properties.envOrElse("TOKEN", "undefined")
 
-  override def run(args: List[String]): IO[ExitCode] =
+  def run(args: List[String]): IO[ExitCode] =
+    Stream
+      .resource(TelegramClient.global[IO](token))
+      .flatMap { implicit client =>
+        Stream.eval(Semaphore[IO](0)).flatMap { sem =>
+          Bot.polling[IO].follow(start(sem), greeting(sem))
+        }
+      }
+      .compile
+      .drain
+      .as(ExitCode.Success)
+
+  def start[F[_]: TelegramClient](semaphore: Semaphore[F]): Scenario[F, Unit] =
     for {
-      _ <- IO.pure(println("CipherDogsBot"))
-    } yield ExitCode.Success
+      chat <- Scenario.expect(command("start").chat)
+      _    <- Scenario.eval(chat.send("CipherDogsBot\nFuck Google! Fuck Twitter! Fuck Web2.0"))
+    } yield ()
+
+  def greeting[F[_]: TelegramClient](semaphore: Semaphore[F]): Scenario[F, Unit] =
+    for {
+      msg <- Scenario.expect(any)
+      _   <- Scenario.eval(greetingBack(msg))
+    } yield ()
+
+  def greetingBack[F[_]: TelegramClient](msg: TelegramMessage): F[_] = msg match {
+    case _: ChatMemberAdded => msg.chat.send("CipherDogsBot\nFuck Google! Fuck Twitter! Fuck Web2.0")
+    case _                  => msg.chat.send("Nop")
+  }
 }
