9b6ccef25d6c1b900d2a3845feffb4336c785102
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-11-27 21:12:06 +0700

    Change Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,11 +1,11 @@
 import sbt._
 
 object Dependencies {
+  lazy val canoe = "org.augustjune" %% "canoe" % "0.5.1"
   lazy val catsCore = "org.typelevel" %% "cats-core" % "2.1.1"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "2.1.4"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.2"
-  lazy val canoe = "org.augustjune" %% "canoe" % "0.5.1"
   lazy val fs2 = "co.fs2" %% "fs2-core" % "2.4.5"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.2"
 
-  val common = Seq(catsCore, catsEffect, scalaTest, canoe, fs2)
+  val common = Seq(canoe, catsCore, catsEffect, fs2, scalaTest)
 }
