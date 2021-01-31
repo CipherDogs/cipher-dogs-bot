f31433cc313bc1099434f8fc8894ce24f2f86bc0
ptfn <CipherDogs@protonmail.com>
2021-01-31 15:26:09 +0700

    fix price

--- src/price.py
+++ src/price.py
@@ -1,12 +1,12 @@
 import requests
 
 def price(arr):
-    r = requests.get('https://www.bw.com/exchange/config/controller/website/pricecontroller/getassistprice')
-    data = r.json()
     string = ''
     for i in range(len(arr)):
         try:
-            price = data['datas']['usd'][arr[i]]
+            r = requests.get('https://api.bitfinex.com/v1/pubticker/'+ arr[i] +'usd')
+            data = r.json()
+            price = data['ask']
             string_coin = arr[i]
             string = string + string_coin.upper() + ': ' + price + '$' + '\n'
         except:
